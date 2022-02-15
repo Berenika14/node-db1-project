87309e43372c155e89be2ac3c5374bf0c451afdc
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2022-02-15 11:13:25 -0500

    endpoints working and wired

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,59 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+router.get("/", (req, res, next) => {
+  try {
+    res.json([{}, {}]);
+  } catch (err) {
+    next({ status: 422, message: "this is horrible" });
+  }
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.put('/:id', (req, res, next) => {
   // DO YOUR MAGIC
 });
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+router.get("/:id", (req, res, next) => {
+  try {
+    res.json("get accounts by id ");
+  } catch (err) {
+    next(err);
+  }
 
-router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
-})
+});
+
+router.post("/", (req, res, next) => {
+  try {
+    res.json("post accounts ");
+  } catch (err) {
+    next(err);
+  }
+
+  // DO YOUR MAGIC
+});
+
+router.put("/:id", (req, res, next) => {
+  try {
+    res.json("update accounts");
+  } catch (err) {
+    next(err);
+  }
+
+  // DO YOUR MAGIC
+});
+
+router.delete("/:id", (req, res, next) => {
+  try {
+    res.json("delete accounts");
+  } catch (err) {
+    next(err);
+  }
+
+  // DO YOUR MAGIC
+});
+
+router.use((err, req, res, next) => {
+  // eslint-disable-line
+  // DO YOUR MAGIC
+  res.status(err.status || 500).json({ message: err.message });
+});
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const accountRouter = require("./accounts/accounts-router");
 
 const server = express();
+
+server.use("/api/accounts", accountRouter);
 
 server.use(express.json());
 server.use("*", (req, res) => {
